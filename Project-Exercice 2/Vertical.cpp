014907db9010c93f98f798aa007a6d885de08eaa
Maxym Blondin <1966592@cstj.qc.ca>
2020-09-14 08:11:22 -0400

    Ajoutez des fichiers projet.

---
+++ Project-Exercice 2/Vertical.cpp
@@ -0,0 +1,45 @@
+// But: Écrire un programme qui vérifie que le nombre entré contient bien 5 chiffres et les affiches verticalement par la suite
+// Nom: Maxym Blondin
+// Date: 2020-09-14
+
+#include <iostream>
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int nombre1;
+	int nombre2 = 9999;
+	
+	std::cout << "Veuillez entrer un nombre à 5 chiffres :";
+	std::cin >> nombre1;
+	// Intégration de la variable nombre 1
+	if (nombre1 >= 10000) // Cette condition permet de s'assurer que le nombre contiendra bel et bien 5 chiffres. En utilisant les variables ''plus petit ou égal que'' et ''plus grand ou égal que'', je peux m'assurer du bon fonctionnement du programme
+		//Ouverture d'un bloc d'instruction qui sera executé seulement si la condition est vraie
+	{
+		std::cout << nombre1 / 10000 <<std::endl; // L'ordinateur ne prennant pas en compte les nombres a virgules (il ne garde que l'entier), il est possible d'obtenir facilement la chiffres a la position des dizaines de miliers, et 
+		std::cout << nombre1 / 1000 % 10 << std::endl;
+		std::cout << nombre1 / 100 % 10 << std::endl;
+		std::cout << nombre1 / 10 % 100 % 10  << std::endl;
+		std::cout << nombre1 / 1 % 1000 % 100 % 10<< std::endl; // En Utilisant la variable Modulo, il est possible d'obtenir le chiffre a la position des unités/dizaines/centaines/miliers. En utilisant la méthode essai-erreur, j'ai pu trouver quels facteurs utiliser dans cette variable
+		//Essai 1 pour mettres les nombres sur une ligne différente: \n (false)
+		//Essai 2 << std::endl; (true)
+	}
+	else
+	{	// Ouverture d'un bloc d'instruction qui sera exécuté seulement si la condition est fausse
+		std::cout << nombre1 << " n'est pas un nombre à 5 chiffres "; 
+	}
+	// Plan de test
+	/*
+	Nombre				5 chiffres			s'affiche sur 5 lignes
+	24376				oui						oui
+	7898				non						non
+	421					non						non
+	14689				oui						oui
+	32					non						non
+
+	Plan de test terminé
+	*/
+
+
+
+}	//Fermeture du bloc d'instruction 
---
+++ Projet - Exercice1/PairImpair.cpp
@@ -0,0 +1,39 @@
+// But: Déterminer si le nombre entier choisi est pair ou impair
+// Auteur: Maxym Blondin
+// Date: 2020-09-14
+
+#include <iostream>
+
+void main()
+{
+	setlocale(LC_ALL, "");
+
+	int nombre1;
+	// Intégration de la variable nombre 1
+	std::cout << "Veuillez entrer un nombre entier :";
+	// Message qui sera affiché pour l'utilisateur
+
+
+	std::cin >> nombre1;
+	if (nombre1 % 2 ==0) // Si le nombre obtient un résultat de 0 lorsqu'il est modulo 2, le nombre est pair. Dans tous les autres cas, il est inévitablement impair
+		//Ouverture d'un bloc d'instruction qui sera executé seulement si la condition est vraie
+	{
+		std::cout << nombre1 << " est un nombre pair. ";
+	}
+
+	else
+	{	// Ouverture d'un bloc d'instruction qui sera exécuté seulement si la condition est fausse
+		std::cout << nombre1 << " n'est pas un nombre pair";
+	}
+	// Plan de test:
+	/*
+		Nombre				Pair ou Impair 
+		6						Pair
+		7						Impair
+		19						Impair
+		20						Pair	
+		3						Impair
+	*/
+
+
+}	//Fermeture du bloc d'instruction 
