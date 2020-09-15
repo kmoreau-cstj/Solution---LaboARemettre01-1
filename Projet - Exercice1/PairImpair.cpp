2079a911f158976cc6e91ce64520a1f126edbb76
Karine Moreau <70488225+kmoreau-cstj@users.noreply.github.com>
2020-09-14 21:44:36 -0400

    Très bon travail
    Des bons commentaires qui expliquent bien ton raisonnement. Parfait !

--- Projet - Exercice1/PairImpair.cpp
+++ Projet - Exercice1/PairImpair.cpp
@@ -1,4 +1,4 @@
-// But: DÈterminer si le nombre entier choisi est pair ou impair
+// But: D√©terminer si le nombre entier choisi est pair ou impair
 // Auteur: Maxym Blondin
 // Date: 2020-09-14
 
@@ -9,23 +9,25 @@ void main()
 	setlocale(LC_ALL, "");
 
 	int nombre1;
-	// IntÈgration de la variable nombre 1
+	// KM : Attention au vocabulaire employ√© : int veut dire integer pour entier. Ici il s'agit d'une d√©claration de variable.
+	// Int√©gration de la variable nombre 1
 	std::cout << "Veuillez entrer un nombre entier :";
-	// Message qui sera affichÈ pour l'utilisateur
+	// Message qui sera affich√© pour l'utilisateur
 
 
 	std::cin >> nombre1;
-	if (nombre1 % 2 ==0) // Si le nombre obtient un rÈsultat de 0 lorsqu'il est modulo 2, le nombre est pair. Dans tous les autres cas, il est inÈvitablement impair
-		//Ouverture d'un bloc d'instruction qui sera executÈ seulement si la condition est vraie
+	if (nombre1 % 2 ==0) // Si le nombre obtient un r√©sultat de 0 lorsqu'il est modulo 2, le nombre est pair. Dans tous les autres cas, il est in√©vitablement impair
+		//Ouverture d'un bloc d'instruction qui sera execut√© seulement si la condition est vraie
 	{
 		std::cout << nombre1 << " est un nombre pair. ";
 	}
 
 	else
-	{	// Ouverture d'un bloc d'instruction qui sera exÈcutÈ seulement si la condition est fausse
+	{	// Ouverture d'un bloc d'instruction qui sera ex√©cut√© seulement si la condition est fausse
 		std::cout << nombre1 << " n'est pas un nombre pair";
 	}
 	// Plan de test:
+	// KM : Bon plan de tests avec de nombreuses valeurs. PEnse √† mettre des valeurs n√©gatives et 0 aussi !
 	/*
 		Nombre				Pair ou Impair 
 		6						Pair
